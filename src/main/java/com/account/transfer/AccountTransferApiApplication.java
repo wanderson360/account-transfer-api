8d41378953dd2139c99d1877c8ee5f13c71d2f66
wanderson360 <wandersondevpy@gmail.com>
2026-06-01 03:20:00 -0300

    chore: add Spring Boot main application and base test class - AccountTransferApiApplication.java with @SpringBootApplication and main method - AccountTransferApiApplicationTests.java with @SpringBootTest and contextLoads() test

---
+++ src/main/java/com/account/transfer/AccountTransferApiApplication.java
@@ -0,0 +1,13 @@
+package com.account.transfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountTransferApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountTransferApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/account/transfer/AccountTransferApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.account.transfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountTransferApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
