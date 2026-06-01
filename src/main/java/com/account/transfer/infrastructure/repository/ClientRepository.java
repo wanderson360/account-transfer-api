f5a60f2cb1514c9ad1624a36759c25c08b5eadff
wanderson360 <wandersondevpy@gmail.com>
2026-06-01 03:06:07 -0300

    feat(infrastructure/repository): add JPA repositories - ClientRepository.java extending JpaRepository<Client, String> - AccountRepository.java extending JpaRepository<Account, String> - TransactionRepository.java extending JpaRepository<Transaction, String> - Added @Repository annotation for each interface

---
+++ src/main/java/com/account/transfer/infrastructure/repository/ClientRepository.java
@@ -0,0 +1,9 @@
+package com.account.transfer.infrastructure.repository;
+
+import com.account.transfer.domain.model.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, String> {
+}
---
+++ src/main/java/com/account/transfer/infrastructure/repository/TransactionRepository.java
@@ -0,0 +1,9 @@
+package com.account.transfer.infrastructure.repository;
+
+import com.account.transfer.domain.model.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, String> {
+}
